e5f38367284d3e5c6fec440fac24b2781dc347ac
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-13 15:43:31 -0400

    Added seed data for all three created tables.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,17 @@
 INSERT INTO department(name)
-VALUES('SALES'),
+VALUES ('SALES'),
+('ENGINEER'),
+('LEGAL'),
 ('HR');
+
+INSERT INTO employee (first_name, last_name)
+VALUES ('Josh', 'Claxton'),
+('Danny', 'Mixon'),
+('Steve', 'McQueen'),
+('MC', 'Hammer');
+
+INSERT INTO role (title, salary)
+VALUES ('Salesperson', '95000'),
+('Lead Engineer', '190000'),
+('Attorney', '125000'),
+('HR Manager', '90000');
